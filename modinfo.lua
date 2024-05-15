8b3181e84727f682036e77d0711a8bdeb2c2f200
rtk0c <mail.tianyig@gmail.com>
2024-05-15 14:14:34 -0700

    Expand hotkey options

--- modinfo.lua
+++ modinfo.lua
@@ -33,35 +33,49 @@ opt_safe_mode = true
 
 local keys = {
   "None",
+-- Alphanumeric keys
   "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
-  "LSHIFT", "LALT", "LCTRL", "TAB", "BACKSPACE", "PERIOD", "SLASH", "TILDE",
+  "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
+  "MINUS", "EQUALS", "SPACE", "ENTER",
+  "TAB", "BACKSPACE", "PERIOD", "COMMA", "SLASH", "BACKSLASH", "SEMICOLON", "LEFTBRACKET", "RIGHTBRACKET", "TILDE",
+  -- These keys tend to have OS-level functionality bound to them, let's not
+  --[["ESCAPE", "PAUSE", "PRINT", "CAPSLOCK", "SCROLLOCK",]]
+  "HOME", "INSERT", "DELETE", "END",
+  "PAGEUP", "PAGEDOWN",
+  "UP", "DOWN", "RIGHT", "LEFT",
+-- Functional row
+  "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",
+-- Modifier keys
+  "LSHIFT", "LCTRL", "LALT", "LSUPER", "RSHIFT", "RCTRL", "RALT", "RSUPER",
+-- Numpad keys
+  "KP_0", "KP_1", "KP_2", "KP_3", "KP_4", "KP_5", "KP_6", "KP_7", "KP_8", "KP_9",
+  "KP_PERIOD", "KP_DIVIDE", "KP_MULTIPLY", "KP_MINUS", "KP_PLUS", "KP_ENTER", "KP_EQUALS",
+-- Mouse buttons
   "Mouse Button 4", "Mouse Button 5"
 }
 
-configuration_options =
-{
+local hotkey_options = {}
+-- We cannot use ipairs because DST's mod loader doesn't provide access to global environment
+-- for _, key in ipairs(keys) do
+for i = 1, #keys do
+  -- Similarly, no table.insert either
+  -- table.insert(hotkey_options, {description = key, data = key})
+  hotkey_options[#hotkey_options + 1] = {description = keys[i], data = keys[i]}
+end
+
+configuration_options = {
   {
     name = "key",
-    label = "上下牛",
-    hover = "快捷上下牛",
-    options = {
-    },
+    label = "Mount/Dismount Beefalo",
+    hover = "Hotkey for mounting and dismounting the closest beefalo.",
+    options = hotkey_options,
     default = "R",
   },
   {
     name = "feed",
-    label = "喂牛",
-    hover = "选择最靠左的食物(Choos the leftmost)",
-    options = {
-    },
+    label = "Feed Beefalo",
+    hover = "Feed beefalo with the leftmost food item in inventory.",
+    options = hotkey_options,
     default = "Mouse Button 4",
   },
 }
-
-local function filltable(tbl)
-  for i = 1, #keys do
-    tbl[i] = {description = keys[i], data = keys[i]}
-  end
-end
-filltable(configuration_options[1].options)
-filltable(configuration_options[2].options)
