8ea41cbe2e94d39cec3894ca2daa278b06f5291d
rtk0c <mail.tianyig@gmail.com>
2024-05-15 20:06:49 -0700

    Better hotreload error handling

--- src/modmain.lua
+++ src/modmain.lua
@@ -2,7 +2,7 @@ modimport("scripts/log")
 modimport("scripts/safe")
 modimport("scripts/modfiles")
 
-local function RunReloadableScripts()
+local function RunReloadableScripts(safe)
   -- Load mod with global environment
   -- Let modfiles to write to the EXPORTS global variable to provide things to this mod environment  
   local old_global_exports = GLOBAL.EXPORTS
@@ -11,11 +11,24 @@ local function RunReloadableScripts()
   for _, modfile in ipairs(modfiles_reloadable) do
     local path = MODROOT .. "scripts/" .. modfile
     local chunk = GLOBAL.kleiloadlua(path)
-    if GLOBAL.type(chunk) == "function" then
+    if safe then
+      -- Bleh, Lua doesn't have continue, so deep nesting in we go!
+      ---- Safety Checks ----
+      if GLOBAL.type(chunk) ~= "function" then
+        LogError("Failed to compile script at " .. path)
+      else
+        GLOBAL.setfenv(chunk, GLOBAL)
+        local status, res = GLOBAL.pcall(chunk)
+        if status then
+          -- No-op
+        else
+          LogError("Failed to load script with error: \n" .. tostring(res))
+        end
+      end
+    else
+      ---- No checks ----
       GLOBAL.setfenv(chunk, GLOBAL)
       chunk()
-    else
-      LogError("Failed to compile script at " .. path)
     end
   end
   GLOBAL.EXPORTS = old_global_exports
@@ -24,8 +37,21 @@ end
 -- Declare global variable EXPORTS with value nil, otherwise no-op
 -- Needed to convince strict.lua that yes, it is fine in this case to access an undeclared variable
 GLOBAL.EXPORTS = GLOBAL.rawget(GLOBAL, "EXPORTS")
--- If errors occur here, just let it fail
-RunReloadableScripts()
+-- Even if safe mode is on in prod:
+-- The mod scripts failed here, so it must be completely faulty, there is no reason to continue at all.
+-- But in dev mode, we might start with faulty code, and wants to iterate on it until it works, and that is much more convenient with hotreloading than keep relaunching the game. As such, we allow faulty scripts to proceed in dev mode.
+if modinfo.opt_dev_mode then
+  RunReloadableScripts(true)
+
+  GLOBAL.clienttweaks_hotreload = function()
+    LogInfo("Reloading")
+    -- Even though console command is already in pcall(), we still want safe mode for per-file error reporting
+    RunReloadableScripts(true)
+    LogInfo("Reload finished")
+  end
+else
+  RunReloadableScripts()
+end
 
 
 
@@ -63,19 +89,3 @@ InstallKeybind(
     if not IsInGameplay() then return end
     Feed()
   end))
-
-
-
-if modinfo.opt_dev_mode then
-  GLOBAL.clienttweaks_hotreload = function()
-    LogInfo("Reloading")
-
-    -- FIXME this really isn't needed, since console commands are wrapped in pcall already
-    local status, res = GLOBAL.pcall(RunReloadableScripts)
-    if status then
-      LogInfo("Reloading completed")
-    else
-      LogError("Reloading failed with error: " .. tostring(res))
-    end
-  end
-end
