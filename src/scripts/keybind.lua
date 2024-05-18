4623b27eadcf2c7f04425bf4ebdc02f75d1db41d
rtk0c <mail.tianyig@gmail.com>
2024-05-18 12:30:15 -0700

    Experiment with adding to all_control directly
    This is easy to get working to add new UI widgets, but still doesn't
    help for the logic of mapping our keybinds. We still need a way of
    hijacking the callbacks created in OptionsScreenn:_BuildControls()
    in order to both update the binding text AND telling MapControl()
    and OnControlMapped().
    
    Also-- I'm not sure how to progmatically set a vanilla control either.
    We need to restore CONTROL_PRIMARY to the original state after our
    hijacked keybind setting is complete. TheInputProxy:CancelMapping()
    seems to be a thing? but it's only ever called in the gamepad codepath.

--- src/scripts/keybind.lua
+++
@@ -1,5 +0,0 @@
-kbd_list = {}
-
-function RegisterKeybind(kbd)
-  table.insert(kbd_list, kbd)
-end
--- src/scripts/reloadable/keybind.lua
+++ src/scripts/reloadable/keybind.lua
@@ -1,14 +1,6 @@
--- Experiment: 
--- - yes that path does refer to the OptionsScreen package
--- - when our mod loads on game startup, the UI is not loaded yet, so this is nil
--- - c_reset() same thing
--- - therefore, most likely during server joins and shard hops it's also the same
--- print("MOD HERE BLAHB BALH"..tostring(package.loaded["screens/redux/optionsscreen"]))
-
+---- FOR DEBUG ----
 local inspect = require "inspect"
-
--- print(OptionsScreen)
--- print(kleiloadlua("scripts/screens/redux/optionsscreen.lua"))
+---- END ----
 
 local class_def = kleiloadlua("scripts/screens/redux/optionsscreen.lua")
 
@@ -21,47 +13,39 @@ local class_all_controls = nil
 
 local env = {}
 local env_metatable = {
-	__index = function(self, key)
-		if key == "Class" then
-			return function(...)
-				local idx = 1
-				while true do
-					local name, value = debug.getlocal(2, idx)
-					if not name then break end
-					if name == "all_controls" then
-						class_all_controls = value
-						break
-					end
-					idx = idx + 1
-				end
-				return _G.Class(...)
-			end
-		end
+  __index = function(t, key)
+    -- Intercept the first time Class() is looked up
+    -- If we already got class_all_controls, no need to intercept anymore, just give them the default thing
+    if key == "Class" and class_all_controls == nil then
+      return function(...)
+        local idx = 1
+        while true do
+          local name, value = debug.getlocal(2, idx)
+          if not name then break end
+          if name == "all_controls" then
+            class_all_controls = value
+            break
+          end
+          idx = idx + 1
+        end
+        return _G.Class(...)
+      end
+    end
 
-		-- If not something we're intercepting, forward to the global environment
-		return _G[key]
-	end,
+    -- If not something we're intercepting, forward to the global environment
+    return _G[key]
+  end,
 
-	__newindex = function(self, key, value)
-		_G[key] = value
-	end
+  __newindex = function(t, key, value)
+    _G[key] = value
+  end
 }
+
 setmetatable(env, env_metatable)
-
--- I think we can safely assume vanilla's code is valid, otherwise the first time won't even load
---[[
-if type(class_def) ~= "function" then
-	print("ERROR: got " .. tostring(class_def) .. " for the class def, expected function")
-	return
-end
---]]
-
 setfenv(class_def, env)
-package.loaded["screens/redux/optionsscreen"] = class_def()
-
--- This successfully removes the first entry, the one for Priamry
-table.remove(class_all_controls, 1)
--- print(inspect(class_all_controls, 2))
+local OptionsScreen = class_def()
+-- Feed this to the package cache, as-if somebody had called require()
+package.loaded["screens/redux/optionsscreen"] = OptionsScreen
 
 -- Now here is a problem: how tf do we _add_ things to the controls menu?
 -- This is roughly how the vanilla logic works:
@@ -82,3 +66,80 @@ table.remove(class_all_controls, 1)
 -- That's also a lot of work, but at least we don't have to figure out how gettext works.
 --
 -- Here ends the diary.
+
+-- Another problem: the control mapping process (the user press a keychord -> store) process is done entirely in TheInputProxy, which is native
+-- Functions like OptionsScreen:OnControlMapped and OptionsScreen:MapControl are just UI hooks
+-- We don't need to add controls to the native system. We just need to hijack the keychord capturing logic, which means hijacking some control (e.g. 0, i.e. Primary) for the keychord capture to start, but display text for our own keybind, and then get the result from TheInputProxy (from the callback added by TheInput:AddControlMappingHandler), and then reset Priamry to its original control
+
+-- TODO figure out how does inputId works, it seems like a 32-bit integer, presumably there're some flag bits in there for modifieres?
+
+-- Using: CONTROL_PRIMARY for hijacking keychord capture
+local is_hijacking_capture = false
+
+-- Notes on Terminology:
+-- Vanilla uses "control" for an action (e.g. Primary, left click by default), we use "keybind"
+-- Vanilla uses "control" for a keychord, we use "keychord"
+
+-- Notes on Control Flow:
+-- The UI widgets are all built in OptionsScreen's ctor call.
+-- The part we care about, the controls tab, is built in OptionsScreen:_BuildControls().
+-- In there, iterate `all_controls` and generate the widgets and callbacks corresponding to each array element.
+-- There are branches for keyboard keybinds and controller keybinds, we only care about the keyboard case. 
+
+kbd_list = {}
+
+local kbd_metatable = {
+  __index = function(t, k)
+    if k == "name" then return t._name end
+    return nil
+  end,
+
+  __newindex = function(t, k, v)
+    if k == "name" then
+      t._name = v
+      STRINGS.UI.CONTROLSSCREEN.CONTROLS[t.string_idx] = v
+    else
+      rawset(t, k, v)
+    end
+  end,
+}
+
+-------
+-- @param id An unique identifier for this keybind.
+-- @param name A human-readable (and preferrably localized) name for this keybind. For dispaly in UI.
+-- @param callback Function to be called when the keybind is triggered.
+function RegisterKeybind(id, name, callback)
+  local string_idx = #STRINGS.UI.CONTROLSSCREEN.CONTROLS + 1
+
+  local kbd = setmetatable(
+    {id = id, string_idx = string_idx, input_mask = 0xFFFFFFFF},
+    kbd_metatable)
+
+  -- Populate these field with the setter; see definition of `kbd_metatable`
+  kbd.name = name
+
+  -- Vanilla reuses the C++ control ID as `name`, except this is 0-indexed, so they +1 for every access into the strings array.
+  -- We have 1-indexed already, -1 to match their code.
+  table.insert(class_all_controls, {name = string_idx-1, keyboard = CONTROL_PRIMARY})
+  table.insert(kbd_list, kbd)
+end
+
+RegisterKeybind("test_kbd", "Test Keybind", function() print("keybind pressed!") end)
+
+local old_MapControl = OptionsScreen.MapControl
+
+function OptionsScreen:MapControl(device_id, control_id)
+  if is_hijacking_capture and control_id == CONTROL_PRIMARY then
+    -- TODO
+  end
+  return old_MapControl(self, device_id, control_id)
+end
+
+local old_OnControlMapped = OptionsScreen.OnControlMapped
+
+function OptionsScreen:OnControlMapped(device_id, control_id, input_id, has_changed)
+  if is_hijacking_capture and control_id == CONTROL_PRIMARY then
+    -- TODO
+  end
+  return old_OnControlMapped(self, device_id, control_id, input_id, has_changed)
+end
