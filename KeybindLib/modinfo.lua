10f59253e8338ed53dc94d3e91fe1259f77e9e7d
rtk0c <mail.tianyig@gmail.com>
2024-05-19 11:33:59 -0700

    Add Steam Workshop URL in modinfo.lua

--- KeybindLib/modinfo.lua
+++ KeybindLib/modinfo.lua
@@ -3,7 +3,7 @@ author = "rtk0c"
 description = [[Library for handling keybinds, in a sane manner. For both users and developers.
 
 See https://github.com/rtk0c/dont-starve-mods/tree/master/KeybindLib for documentation.
-This mod is published on Steam Workshop TODO.]]
+This mod is published at https://fontawesome.com/icons/keyboard?f=classic&s=solid on Steam Workshop.]]
 version = "1.0"
 
 dont_starve_compatible = false
