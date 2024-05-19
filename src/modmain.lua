97bf0965ebb68007a3f3d92871efa5f8e6ce0f27
rtk0c <mail.tianyig@gmail.com>
2024-05-18 19:32:31 -0700

    Convert beefalokeybind.lua functionality to keybind system

--- src/modmain.lua
+++ src/modmain.lua
@@ -56,27 +56,6 @@ end
 
 
 
-local function InstallMouseKeybind(mouse_keycode, callback)
-  GLOBAL.TheInput:AddMouseButtonHandler(function (button, down, x, y)
-    if button == mouse_keycode and down then
-      callback(button, down, x, y)
-    end
-  end)
-end
-
-local function InstallKeybind(config_key, callback)
-  if config_key == "None" then return end
-
-  if config_key == "Mouse Button 4" then
-    return InstallMouseKeybind(1005, callback)
-  elseif config_key == "Mouse Button 5" then
-    return InstallMouseKeybind(1006, callback)
-  end
-
-  local keycode = GLOBAL["KEY_"..config_key]
-  GLOBAL.TheInput:AddKeyDownHandler(keycode, callback)
-end
-
 --[[
 GLOBAL.TheInput:AddMouseButtonHandler(function(button, down, x, y))
   print("Mouse button: button="..button.." down="..tostring(down).." x="..x.." y="..y)
@@ -86,16 +65,24 @@ GLOBAL.TheInput:AddKeyHandler(function(key, down)
 end)
 --]]
 
-InstallKeybind(
-  GetModConfigData("key"),
-  SafeWrapper(function()
+RegisterKeybind({
+  id = "mount_dismount",
+  name = "Mount/Dismount Beefalo",
+  description = "Hotkey for mounting and dismounting the closest beefalo.",
+  modid = modinfo.id,
+  callback = SafeWrapper(function()
     if not IsInGameplay() then return end
     MountOrDis()
-  end))
+  end),
+})
 
-InstallKeybind(
-  GetModConfigData("feed"),
-  SafeWrapper(function()
+RegisterKeybind({
+  id = "feed",
+  name = "Feed Beefalo",
+  description = "Feed beefalo with the leftmost food item in inventory.",
+  modid = modinfo.id,
+  callback = SafeWrapper(function()
     if not IsInGameplay() then return end
     Feed()
-  end))
+  end),
+})
