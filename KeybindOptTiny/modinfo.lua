eb1b6ce64b86ccc81575bdfd0a50917941fe70d5
rtk0c <mail.tianyig@gmail.com>
2024-07-10 09:13:03 -0700

    KeybindOptTiny [2/n]

--- KeybindOptTiny/modinfo.lua
+++ KeybindOptTiny/modinfo.lua
@@ -13,3 +13,55 @@ api_version = 10
 all_clients_require_mod = false
 server_only_mod = false
 client_only_mod = true
+
+-- Function picking the correct local string
+local T = ChooseTranslationTable
+
+-- Generate a list of all available keys
+-- From https://github.com/liolok/RangeIndicator/blob/master/modinfo.lua
+keys = { -- from STRINGS.UI.CONTROLSSCREEN.INPUTS[1] of strings.lua, need to match constants.lua too.
+  'Disabled', 'Escape', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'Print', 'ScrolLock', 'Pause',
+  '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+  'Tab', 'CapsLock', 'LShift', 'LCtrl', 'LAlt', 'Space', 'RAlt', 'RCtrl', 'Period', 'Slash', 'RShift',
+  'Minus', 'Equals', 'Backspace', 'LeftBracket', 'RightBracket', 'Backslash', 'Semicolon', 'Enter',
+  'Up', 'Down', 'Left', 'Right', 'Insert', 'Delete', 'Home', 'End', 'PageUp', 'PageDown', -- navigation
+  'Num 0', 'Num 1', 'Num 2', 'Num 3', 'Num 4', 'Num 5', 'Num 6', 'Num 7', 'Num 8', 'Num 9', -- numberic keypad
+  'Num Period', 'Num Divide', 'Num Multiply', 'Num Minus', 'Num Plus', 'Disabled',
+}
+for i = 1, #keys do
+  local k = keys[i]
+  keys[i] = {
+    description = k,
+    data = 'KEY_' .. k:gsub('^Num ', 'KP_'):upper(),
+  }
+end
+
+configuration_options = {}
+
+local keybind_list = {
+  -- For terseness in this demo, I put extra info into the translation table (first 3 entries here)
+  -- In practice you should probably take them out
+  { _name = "my_alice",   _default_key = "KEY_F",   "Alice", zh="爱丽丝", zht="愛麗絲" },
+  { _name = "my_bob",     _default_key = "KEY_G",   "Bob",   zh="鲍勃",   zht="鮑勃" },
+  { _name = "my_carol",   _default_key = "KEY_H",   "Carol", zh="卡罗",   zht="卡羅" },
+}
+-- Do 2 things:
+-- 1. Generate config options
+-- 2. Turn the array into a hashtable
+--    We have to use an array in modinfo.lua, because pairs() isn't available, so iterating a hashtable is not possible
+keybinds = {}
+for i = 1, #keybind_list do
+  local kbd = keybind_list[i]
+  local config_idx = #configuration_options + 1
+  configuration_options[config_idx] = {
+    name = kbd._name,
+    label = T(kbd),
+    default = kbd._default_key,
+    options = keys,
+  }
+
+  keybinds[kbd._name] = kbd
+  kbd._config_idx = config_idx
+  kbd._name = nil
+end
--- KeybindOptTiny/modmain.lua
+++ KeybindOptTiny/modmain.lua
@@ -1,20 +1,60 @@
-modimport("scripts/keybind")
+modimport("scripts/keybind_magic")
 
-local function MyKeybindHandler()
-  print("here")
+-- ChooseTranslationTable() provided in modindex.lua
+-- Not available outside of modinfo.lua but we still need it
+local function T(tbl)
+  local locale = GLOBAL.LOC.GetLocaleCode()
+  return tbl[locale] or tbl[1]
 end
 
--- Load the default option, somehow
--- Maybe from configuration_options? maybe your own config storage system?
-local my_keybind_eh = GLOBAL.TheInput:AddKeyDownHandler(GLOBAL.KEY_B, MyKeybindHandler)
+local keycode2key = { [0] = "KEY_DISABLED" }
+local rawget = GLOBAL.rawget
+for _, key_option in pairs(modinfo.keys) do
+  local varname = key_option.data
+  if varname ~= "KEY_DISABLED" then
+    keycode2key[rawget(GLOBAL, varname)] = varname
+  end
+end
+local function StringifyKeycode(keycode)
+  return keycode2key[keycode]
+end
+local function ParseKeyString(key)
+  return key == "KEY_DISABLED" and 0 or rawget(GLOBAL, key)
+end
 
-KeybindHandler:Add("My keybind", GLOBAL.KEY_B,
-  function(new_key)
-    local g_TI = GLOBAL.TheInput
-    g_TI.onkeydown:RemoveHandler(my_keybind_eh)
+local key_handlers = {}
+
+-- We have now defined keybinds in modinfo.lua
+-- Reify them by assigning a callback to each
+
+local modactualname = GLOBAL.KnownModIndex:GetModActualName(modinfo.name)
+
+local function AddKeybind(id, handler)
+  local kbd = modinfo.keybinds[id]
+  local curr_keycode = ParseKeyString(GetModConfigData(id))
+  local def_keycode = ParseKeyString(kbd._default_key)
+
+  -- Add the initial key handler
+  if new_key ~= 0 then
+    key_handlers[id] = GLOBAL.TheInput:AddKeyDownHandler(curr_keycode, handler)
+  end
+
+  KeybindHandler:Add(T(kbd), def_keycode, curr_keycode, function(new_key)
+    -- Update key handler
+    GLOBAL.TheInput.onkeydown:RemoveHandler(key_handlers[id])
     if new_key ~= 0 then
-      my_keybind_eh = g_TI:AddKeyDownHandler(new_key, MyKeybindHandler)
+      key_handlers[id] = GLOBAL.TheInput:AddKeyDownHandler(new_key, handler)
     else
-      my_keybind_eh = nil
+      key_handlers[id] = nil
     end
+
+    -- Update mod config
+    local config = GLOBAL.KnownModIndex:LoadModConfigurationOptions(modactualname, true)
+    config[kbd._config_idx].saved = StringifyKeycode(new_key)
+		GLOBAL.KnownModIndex:SaveConfigurationOptions(function() end, modactualname, config, true)
   end)
+end
+
+AddKeybind("my_alice", function() print("Alice here!") end)
+AddKeybind("my_bob", function() print("Bob here!") end)
+AddKeybind("my_carol", function() print("Carol here!") end)
